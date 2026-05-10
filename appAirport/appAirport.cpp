3523b5d97eb4fd09eb5b5779d2c43240c3ae811f
VladimirFilippov10 <asdqwer11zx@gmail.com>
2026-05-10 20:23:07 +0500

    интерфейс итог1

--- appAirport/appAirport.cpp
+++ appAirport/appAirport.cpp
@@ -96,7 +96,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
     hInst = hInstance;
     HWND hWnd = CreateWindowW(szWindowClass, L"Аэропорт - Система управления рейсами",
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 1400, 950, nullptr, nullptr, hInstance, nullptr);
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 1450, 950, nullptr, nullptr, hInstance, nullptr);
     if (!hWnd) return FALSE;
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
@@ -111,11 +111,11 @@ void CreateControls(HWND hWnd) {
     // ========== ТАБЛИЦА РЕЙСОВ ==========
     hListFlights = CreateWindowW(WC_LISTVIEW, NULL,
         WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL,
-        10, 10, 950, 320, hWnd, (HMENU)IDC_LIST_FLIGHTS, hInst, NULL);
+        10, 10, 980, 320, hWnd, (HMENU)IDC_LIST_FLIGHTS, hInst, NULL);
 
     LVCOLUMNW lvc = { LVCF_TEXT | LVCF_WIDTH };
     const wchar_t* cols[] = { L"Номер рейса", L"Назначение", L"Отправление", L"Время вылета", L"Длит.(мин)", L"Мест" };
-    int widths[] = { 130, 170, 170, 210, 100, 90 };
+    int widths[] = { 140, 180, 180, 220, 100, 90 };
     for (int i = 0; i < 6; i++) {
         lvc.cx = widths[i];
         lvc.pszText = const_cast<wchar_t*>(cols[i]);
@@ -124,102 +124,102 @@ void CreateControls(HWND hWnd) {
 
     // ========== ПАНЕЛЬ ДОБАВЛЕНИЯ РЕЙСА ==========
     CreateWindowW(L"STATIC", L"ДОБАВЛЕНИЕ РЕЙСА", WS_CHILD | WS_VISIBLE | SS_CENTER,
-        980, 10, 370, 30, hWnd, NULL, hInst, NULL);
+        1010, 10, 390, 30, hWnd, NULL, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 980, 50, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditFlightNum = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 50, 240, 30, hWnd, (HMENU)IDC_EDIT_FLIGHT_NUM, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 1010, 50, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditFlightNum = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 50, 260, 30, hWnd, (HMENU)IDC_EDIT_FLIGHT_NUM, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Назначение:", WS_CHILD | WS_VISIBLE, 980, 90, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditDest = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 90, 240, 30, hWnd, (HMENU)IDC_EDIT_DEST, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Назначение:", WS_CHILD | WS_VISIBLE, 1010, 90, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditDest = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 90, 260, 30, hWnd, (HMENU)IDC_EDIT_DEST, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Отправление:", WS_CHILD | WS_VISIBLE, 980, 130, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditDepart = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 130, 240, 30, hWnd, (HMENU)IDC_EDIT_DEPART, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Отправление:", WS_CHILD | WS_VISIBLE, 1010, 130, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditDepart = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 130, 260, 30, hWnd, (HMENU)IDC_EDIT_DEPART, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Время (ГГГГ-ММ-ДД ЧЧ:ММ):", WS_CHILD | WS_VISIBLE, 980, 170, 200, 30, hWnd, NULL, hInst, NULL);
-    hEditTime = CreateWindowW(L"EDIT", L"2025-05-10 08:00", WS_CHILD | WS_VISIBLE | WS_BORDER, 1190, 170, 160, 30, hWnd, (HMENU)IDC_EDIT_TIME, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Время (ГГГГ-ММ-ДД ЧЧ:ММ):", WS_CHILD | WS_VISIBLE, 1010, 170, 200, 30, hWnd, NULL, hInst, NULL);
+    hEditTime = CreateWindowW(L"EDIT", L"2025-05-10 08:00", WS_CHILD | WS_VISIBLE | WS_BORDER, 1220, 170, 180, 30, hWnd, (HMENU)IDC_EDIT_TIME, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Длительность (мин):", WS_CHILD | WS_VISIBLE, 980, 210, 130, 30, hWnd, NULL, hInst, NULL);
-    hEditDuration = CreateWindowW(L"EDIT", L"120", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 210, 120, 30, hWnd, (HMENU)IDC_EDIT_DURATION, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Длительность (мин):", WS_CHILD | WS_VISIBLE, 1010, 210, 130, 30, hWnd, NULL, hInst, NULL);
+    hEditDuration = CreateWindowW(L"EDIT", L"120", WS_CHILD | WS_VISIBLE | WS_BORDER, 1150, 210, 130, 30, hWnd, (HMENU)IDC_EDIT_DURATION, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Кол-во мест:", WS_CHILD | WS_VISIBLE, 980, 250, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditSeats = CreateWindowW(L"EDIT", L"50", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 250, 120, 30, hWnd, (HMENU)IDC_EDIT_SEATS, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Кол-во мест:", WS_CHILD | WS_VISIBLE, 1010, 250, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditSeats = CreateWindowW(L"EDIT", L"50", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 250, 130, 30, hWnd, (HMENU)IDC_EDIT_SEATS, hInst, NULL);
 
     CreateWindowW(L"BUTTON", L"+ ДОБАВИТЬ РЕЙС", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        980, 295, 170, 40, hWnd, (HMENU)IDC_BTN_ADD_FLIGHT, hInst, NULL);
+        1010, 295, 180, 40, hWnd, (HMENU)IDC_BTN_ADD_FLIGHT, hInst, NULL);
     CreateWindowW(L"BUTTON", L"- УДАЛИТЬ РЕЙС", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        1165, 295, 170, 40, hWnd, (HMENU)IDC_BTN_DEL_FLIGHT, hInst, NULL);
+        1205, 295, 180, 40, hWnd, (HMENU)IDC_BTN_DEL_FLIGHT, hInst, NULL);
 
-    // ========== ПАНЕЛЬ УПРАВЛЕНИЯ ==========
+    // ========== ПАНЕЛЬ УПРАВЛЕНИЯ (РАСШИРЕНА) ==========
     CreateWindowW(L"BUTTON", L"ЗАГРУЗИТЬ ДАННЫЕ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 345, 140, 40, hWnd, (HMENU)IDC_BTN_LOAD, hInst, NULL);
+        10, 345, 160, 40, hWnd, (HMENU)IDC_BTN_LOAD, hInst, NULL);
     CreateWindowW(L"BUTTON", L"СОХРАНИТЬ ДАННЫЕ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        160, 345, 140, 40, hWnd, (HMENU)IDC_BTN_SAVE, hInst, NULL);
+        180, 345, 160, 40, hWnd, (HMENU)IDC_BTN_SAVE, hInst, NULL);
     CreateWindowW(L"BUTTON", L"ОЧИСТИТЬ ВСЁ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        310, 345, 140, 40, hWnd, (HMENU)IDC_BTN_CLEAR, hInst, NULL);
+        350, 345, 160, 40, hWnd, (HMENU)IDC_BTN_CLEAR, hInst, NULL);
 
     // ========== ЗАПРОСЫ ==========
     CreateWindowW(L"STATIC", L"ЗАПРОСЫ", WS_CHILD | WS_VISIBLE | SS_CENTER,
-        10, 400, 650, 30, hWnd, NULL, hInst, NULL);
+        10, 400, 700, 30, hWnd, NULL, hInst, NULL);
 
     // Запрос 1
     CreateWindowW(L"BUTTON", L"Свободные места", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 440, 140, 35, hWnd, (HMENU)IDC_BTN_CHECK_SEATS, hInst, NULL);
-    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 165, 445, 90, 30, hWnd, NULL, hInst, NULL);
-    hEditQueryFlight = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 265, 445, 150, 30, hWnd, (HMENU)IDC_EDIT_QUERY_FLIGHT, hInst, NULL);
+        10, 440, 150, 35, hWnd, (HMENU)IDC_BTN_CHECK_SEATS, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 175, 445, 90, 30, hWnd, NULL, hInst, NULL);
+    hEditQueryFlight = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 275, 445, 160, 30, hWnd, (HMENU)IDC_EDIT_QUERY_FLIGHT, hInst, NULL);
 
     // Запрос 2
     CreateWindowW(L"BUTTON", L"По направлению", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 485, 140, 35, hWnd, (HMENU)IDC_BTN_FIND_DEST, hInst, NULL);
-    CreateWindowW(L"STATIC", L"Город:", WS_CHILD | WS_VISIBLE, 165, 490, 90, 30, hWnd, NULL, hInst, NULL);
-    hEditQueryDest = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 265, 490, 150, 30, hWnd, (HMENU)IDC_EDIT_QUERY_DEST, hInst, NULL);
+        10, 485, 150, 35, hWnd, (HMENU)IDC_BTN_FIND_DEST, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Город:", WS_CHILD | WS_VISIBLE, 175, 490, 90, 30, hWnd, NULL, hInst, NULL);
+    hEditQueryDest = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 275, 490, 160, 30, hWnd, (HMENU)IDC_EDIT_QUERY_DEST, hInst, NULL);
 
     // Запрос 3
     CreateWindowW(L"BUTTON", L"Макс.цена билета", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 530, 140, 35, hWnd, (HMENU)IDC_BTN_MAX_PRICE, hInst, NULL);
-    CreateWindowW(L"STATIC", L"(используйте номер рейса выше)", WS_CHILD | WS_VISIBLE, 165, 535, 220, 30, hWnd, NULL, hInst, NULL);
+        10, 530, 150, 35, hWnd, (HMENU)IDC_BTN_MAX_PRICE, hInst, NULL);
+    CreateWindowW(L"STATIC", L"(используйте номер рейса выше)", WS_CHILD | WS_VISIBLE, 175, 535, 230, 30, hWnd, NULL, hInst, NULL);
 
     // Запрос 4
     CreateWindowW(L"BUTTON", L"Ближайший рейс", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 575, 140, 35, hWnd, (HMENU)IDC_BTN_NEAREST, hInst, NULL);
-    CreateWindowW(L"STATIC", L"(используйте город выше)", WS_CHILD | WS_VISIBLE, 165, 580, 200, 30, hWnd, NULL, hInst, NULL);
+        10, 575, 150, 35, hWnd, (HMENU)IDC_BTN_NEAREST, hInst, NULL);
+    CreateWindowW(L"STATIC", L"(используйте город выше)", WS_CHILD | WS_VISIBLE, 175, 580, 210, 30, hWnd, NULL, hInst, NULL);
 
     // Сохранение результата
     CreateWindowW(L"BUTTON", L"СОХРАНИТЬ РЕЗУЛЬТАТ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10, 625, 180, 40, hWnd, (HMENU)IDC_BTN_SAVE_QUERY, hInst, NULL);
+        10, 625, 200, 40, hWnd, (HMENU)IDC_BTN_SAVE_QUERY, hInst, NULL);
 
     // ========== ТАБЛИЦА РЕЗУЛЬТАТОВ ==========
     CreateWindowW(L"STATIC", L"РЕЗУЛЬТАТЫ ЗАПРОСОВ", WS_CHILD | WS_VISIBLE | SS_CENTER,
-        10, 680, 650, 30, hWnd, NULL, hInst, NULL);
+        10, 680, 700, 30, hWnd, NULL, hInst, NULL);
 
     hListResults = CreateWindowW(WC_LISTVIEW, NULL,
         WS_CHILD | WS_VISIBLE | LVS_REPORT,
-        10, 715, 950, 100, hWnd, (HMENU)IDC_LIST_RESULTS, hInst, NULL);
-    lvc.cx = 930;
+        10, 715, 980, 100, hWnd, (HMENU)IDC_LIST_RESULTS, hInst, NULL);
+    lvc.cx = 960;
     lvc.pszText = const_cast<wchar_t*>(L"Результат");
     ListView_InsertColumn(hListResults, 0, &lvc);
 
     // ========== ПРОДАЖА БИЛЕТОВ ==========
     CreateWindowW(L"STATIC", L"ПРОДАЖА БИЛЕТОВ", WS_CHILD | WS_VISIBLE | SS_CENTER,
-        980, 345, 370, 30, hWnd, NULL, hInst, NULL);
+        1010, 345, 390, 30, hWnd, NULL, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 980, 390, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditTicketFlight = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 390, 240, 30, hWnd, (HMENU)IDC_EDIT_TICKET_FLIGHT, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Номер рейса:", WS_CHILD | WS_VISIBLE, 1010, 390, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditTicketFlight = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 390, 260, 30, hWnd, (HMENU)IDC_EDIT_TICKET_FLIGHT, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Пассажир (ФИО):", WS_CHILD | WS_VISIBLE, 980, 430, 110, 30, hWnd, NULL, hInst, NULL);
-    hEditPassenger = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1100, 430, 280, 30, hWnd, (HMENU)IDC_EDIT_PASSENGER, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Пассажир (ФИО):", WS_CHILD | WS_VISIBLE, 1010, 430, 110, 30, hWnd, NULL, hInst, NULL);
+    hEditPassenger = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1130, 430, 290, 30, hWnd, (HMENU)IDC_EDIT_PASSENGER, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Цена (руб):", WS_CHILD | WS_VISIBLE, 980, 470, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditPrice = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 470, 180, 30, hWnd, (HMENU)IDC_EDIT_PRICE, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Цена (руб):", WS_CHILD | WS_VISIBLE, 1010, 470, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditPrice = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 470, 200, 30, hWnd, (HMENU)IDC_EDIT_PRICE, hInst, NULL);
 
-    CreateWindowW(L"STATIC", L"Касса №:", WS_CHILD | WS_VISIBLE, 980, 510, 100, 30, hWnd, NULL, hInst, NULL);
-    hEditTicketCashier = CreateWindowW(L"EDIT", L"001", WS_CHILD | WS_VISIBLE | WS_BORDER, 1090, 510, 100, 30, hWnd, (HMENU)IDC_EDIT_TICKET_CASHIER, hInst, NULL);
+    CreateWindowW(L"STATIC", L"Касса №:", WS_CHILD | WS_VISIBLE, 1010, 510, 100, 30, hWnd, NULL, hInst, NULL);
+    hEditTicketCashier = CreateWindowW(L"EDIT", L"001", WS_CHILD | WS_VISIBLE | WS_BORDER, 1120, 510, 120, 30, hWnd, (HMENU)IDC_EDIT_TICKET_CASHIER, hInst, NULL);
 
     CreateWindowW(L"BUTTON", L"ПРОДАТЬ БИЛЕТ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        1090, 560, 200, 45, hWnd, (HMENU)IDC_BTN_SELL_TICKET, hInst, NULL);
+        1120, 560, 240, 45, hWnd, (HMENU)IDC_BTN_SELL_TICKET, hInst, NULL);
 
     // Информационная строка
     CreateWindowW(L"STATIC", L"Совет: Для запросов Макс.цена и Ближайший рейс используйте номер рейса и город из полей выше",
-        WS_CHILD | WS_VISIBLE, 10, 830, 900, 30, hWnd, NULL, hInst, NULL);
+        WS_CHILD | WS_VISIBLE, 10, 830, 1000, 30, hWnd, NULL, hInst, NULL);
 }
 
 // Обработчик сообщений
